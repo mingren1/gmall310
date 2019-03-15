e1e4207e1f2cdae129af193984c7ea2a566416ce
wang <18522531324@163.com>
2019-03-15 14:11:45 +0800

    增加文件上传和下载项目

---
+++ upanddownload/src/main/java/com/athome/gmall/upanddownload/UpanddownloadApplication.java
@@ -0,0 +1,13 @@
+package com.athome.gmall.upanddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpanddownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpanddownloadApplication.class, args);
+	}
+
+}
